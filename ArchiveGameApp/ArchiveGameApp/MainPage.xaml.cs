fd7f796d1ea0b528d4b12601b5c947e0980b3212
emil38a7 <emilk@DESKTOP-I0SDNNG>
2018-05-25 13:42:45 +0200

    post Player

--- ArchiveGameApp/ArchiveGameApp/MainPage.xaml.cs
+++ ArchiveGameApp/ArchiveGameApp/MainPage.xaml.cs
@@ -9,9 +9,30 @@ namespace ArchiveGameApp
 {
 	public partial class MainPage : ContentPage
 	{
+        httpService http;
 		public MainPage()
 		{
 			InitializeComponent();
+            http = new httpService();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Player player = new Player();
+            System.Diagnostics.Debug.WriteLine("Hi***********************************");
+            this.http.PostNewPlayer(new Player());
+        }
+    }
+
+    public class Player
+    {
+        string playerID;
+        string playerNickName;
+
+        public Player()
+        {
+            playerID = "10";
+            playerNickName = "Bob";
+        }
+    }
 }
---
+++ ArchiveGameApp/ArchiveGameApp/httpService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ArchiveGameApp
+{
+    class httpService
+    {
+        HttpClient client;
+
+
+        string postPlayerUrl = "http://192.168.1.144:3000/player";
+
+        public httpService()
+        {
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+        }
+
+        public async void PostNewPlayer(Object player)
+        {
+            //   var player = new { playerNickName = nameEntry.Text };
+            var json = JsonConvert.SerializeObject(player);
+            //var json = "{\"playerNickName\": \"" + player.Text + "\"}";
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync(postPlayerUrl, content);
+                System.Diagnostics.Debug.WriteLine(response.ToString());
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Alert ", e.ToString());
+            }
+        }
+    }
+}
